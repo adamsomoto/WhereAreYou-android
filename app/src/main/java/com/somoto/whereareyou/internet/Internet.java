f08cbb03f65722d07d8a31d183ac5958c38b9e5e
adam <adam@somote.net>
2016-10-30 18:20:57 +0200

    SendActivity

--- app/src/main/java/com/somoto/whereareyou/internet/Internet.java
+++ app/src/main/java/com/somoto/whereareyou/internet/Internet.java
@@ -1,7 +1,28 @@
 package com.somoto.whereareyou.internet;
 
+import android.content.Context;
+import android.content.Intent;
+
+import com.somoto.whereareyou.R;
+
+import java.util.Random;
+
 public class Internet {
 
     public static final String HOST = "https://whereareyou-148008.appspot.com";
+
+    public static void invite(Context context){
+        Random rand = new Random();
+        int umid = rand.nextInt(9999);
+        String link = Internet.HOST+"/share_loc.html?umid="+umid;
+        String message = context.getString(R.string.message);
+        String fullMessage = message+" "+link;
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, fullMessage);
+        sendIntent.setType("text/plain");
+        context.startActivity(sendIntent);
+        //TODO write the umid in SharedPrefs
+    }
 
 }
