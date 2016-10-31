ad54ad7059a5fd97dd44d21135672d4eccc1272e
umi <umitay@genieo.com>
2016-10-31 09:19:19 +0200

    added google maps activity and multidex enabled

--- app/src/main/java/com/somoto/whereareyou/ui/MapsActivity.java
+++ app/src/main/java/com/somoto/whereareyou/ui/MapsActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.somoto.whereareyou.R;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
